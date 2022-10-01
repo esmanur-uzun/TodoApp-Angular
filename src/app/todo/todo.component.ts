50b9e00cac867592ad13e06082936b61a65003e2
esmanurbtmz <esmabitmez@hotmail.com>
2022-10-01 22:02:34 +0300

    style changed

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent{
 
   constructor() { }
-
-  ngOnInit(): void {
+  addItem(){
+    console.log("merhaba")
   }
 
 }
