deed14aa2f49ffd6eaa3c8af13efaf34fcedb09b
esmanurbtmz <esmabitmez@hotmail.com>
2022-10-03 14:58:10 +0300

    project completed

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -10,13 +10,20 @@ export class TodoComponent{
   todos: Todo[] = []
   newTodo:string
 
+  constructor(){
+    this.todos = this.getItemFromLS()
+  }
+
   saveTodo(){
     if(this.newTodo){
       let todo = new Todo()
       todo.name=this.newTodo
       todo.isCompleted= true
 
+      
+      this.todos = this.getItemFromLS()
       this.todos.push(todo)
+      localStorage.setItem("todos", JSON.stringify(this.todos))
 
       this.newTodo = ""
     }
@@ -27,5 +34,31 @@ export class TodoComponent{
 
   done(id:number){
     this.todos[id].isCompleted = !this.todos[id].isCompleted
+    localStorage.setItem("todos", JSON.stringify(this.todos))
+  }
+
+  remove(id:number){
+    this.todos = this.todos.filter((v,i) => i !== id )
+    localStorage.setItem("todos", JSON.stringify(this.todos))
+  }
+  
+  getItems(){
+    return this.todos
+  }
+
+  clearAll(){
+    this.todos= []
+    localStorage.setItem("todos", JSON.stringify(this.todos))
+  }
+  getItemFromLS(){
+   
+    
+    let value = localStorage.getItem("todos")
+
+    if(value != null){
+      this.todos= JSON.parse(value)
+    }
+    return this.todos;
+
   }
 }
